7b867637d1c115df762584c603052f0a924749e3
Lucas De Marchi <lucas.demarchi@intel.com>
2021-06-30 00:55:43 -0700

    setup: first try at genbranch
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- setup/run.sh
+++ setup/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-env
-echo "Arguments:" "$@"
-git pile --version
+set -eu
+
+PILE_BRANCH=$1
+PILE_REF=$2
+RESULT_BRANCH=$3
+FETCH_BASELINE=$4
+
+. config
+git pile destroy || true
+git pile init -p $PILE_BRANCH -b $BASELINE -r $RESULT_BRANCH
+git -C patches reset --hard $PILE_REF
+
+[ "$FETCH_BASELINE" == "true" ] && git fetch -f origin $BASELINE
+git branch -f $RESULT_BRANCH $BASELINE
+
+git pile genbranch -i
+git log
